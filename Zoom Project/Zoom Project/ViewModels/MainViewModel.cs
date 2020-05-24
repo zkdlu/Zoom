6f4f43a0ea93c4390b26c7d56b9569daf867287c
zkdlu <zkdlu159951@gmail.com>
2020-05-24 11:23:31 +0900

    zoom project

--- Zoom Project/Zoom Project/ViewModels/MainViewModel.cs
+++ Zoom Project/Zoom Project/ViewModels/MainViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using GalaSoft.MvvmLight.Command;
 using System;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
+using Zoom_Project.API;
+using Zoom_Project.Models;
 
 namespace Zoom_Project.ViewModels
 {
@@ -48,8 +51,92 @@ namespace Zoom_Project.ViewModels
 
         private void OpenWindowOrStop()
         {
-            ShareWindow shareWindow = new ShareWindow();
-            shareWindow.ShowDialog();
+            if (!Mediator.IsShare)
+            {
+                ShareWindow shareWindow = new ShareWindow();
+                shareWindow.Closed += ShareWindow_Closed;
+                shareWindow.ShowDialog();
+            }
+            else
+            {
+                StopShare();
+            }
+        }
+
+        private void ShareWindow_Closed(object sender, EventArgs e)
+        {
+            if (Mediator.IsShare)
+            {
+                StartShare();
+            }
+        }
+
+        private void StopShare()
+        {
+            Mediator.IsShare = false;
+
+            if (_dispatcherTimer != null)
+            {
+                _dispatcherTimer.Stop();
+                _dispatcherTimer = null;
+            }
+
+            ButtonConent = "Share";
+            ShareImage = null;
+        }
+
+        private void StartShare()
+        {
+            if (_dispatcherTimer == null)
+            {
+                _dispatcherTimer = new DispatcherTimer();
+                _dispatcherTimer.Interval = TimeSpan.FromMilliseconds(1);
+                _dispatcherTimer.Tick += _dispatcherTimer_Tick;
+                _dispatcherTimer.Start();
+            }
+
+            ButtonConent = "Stop";
+        }
+
+        private void _dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (!Mediator.IsShare)
+            {
+                return;
+            }
+
+            switch (Mediator.ShareMode)
+            {
+                case ShareMode.Screen:
+                    ShareScreen();
+                    break;
+                case ShareMode.Application:
+                    ShareApplication();
+                    break;
+                case ShareMode.None:
+                    StopShare();
+                    break;
+            }
+        }
+
+        private void ShareApplication()
+        {
+            ProcessInfo processInfo = Mediator.Application;
+            if (processInfo == null)
+            {
+                MessageBox.Show("공유 할 프로세스를 선택해주세요.");
+
+                StopShare();
+
+                return;
+            }
+
+            ShareImage = Capture.ScreenshotWindow(processInfo.ProcessHandle);
+        }
+
+        private void ShareScreen()
+        {
+            ShareImage = Capture.ScreenshotScreen();
         }
     }
 }
